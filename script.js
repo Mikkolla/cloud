2b3e4f0caae2a6c6a7584e759d1bef59b96a7a7c
Mikkolla <kamenevkola05@gmail.com>
2024-03-16 00:06:12 +0200

    init repo

---
+++ script.js
@@ -0,0 +1,15 @@
+function navToggle() {
+	document.getElementById("headerUl").classList.toggle("_active");
+	
+}
+
+function validate(evt) {
+	var theEvent = evt || window.event;
+	var key = theEvent.keyCode || theEvent.which;
+	key = String.fromCharCode( key );
+	var regex = /[0-9]|\./;
+	if( !regex.test(key) ) {
+	  theEvent.returnValue = false;
+	  if(theEvent.preventDefault) theEvent.preventDefault();
+	}
+  }
